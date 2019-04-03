0b9304a62ae56c8c5504d863735ef9756bcda6be
ifernandezblanco <ifernandezblanco@nautilus29.danielcastelao.org>
2019-04-03 10:27:09 +0200

    Pro-Boletin-26 revisado y corregido

---
+++ Boletin-26/src/boletin/pkg26/Boletin26.java
@@ -0,0 +1,16 @@
+
+package boletin.pkg26;
+
+/**
+ *
+ * @author ifernandezblanco
+ */
+public class Boletin26 {
+
+   
+    public static void main(String[] args) {
+        Ventana26 obxVen = new Ventana26();
+        obxVen.setVisible(true);
+    }
+    
+}
